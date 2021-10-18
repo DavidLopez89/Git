b433d6e399af8d787a6866fc8460715785391ce8
DavidLopez89 <iron_lopez89@hotmail.com>
2021-10-18 13:14:27 +0200

    añado estoy en jorge

--- Program.vb
+++ Program.vb
@@ -3,6 +3,7 @@ Imports System
 Module Program
     '18/10/2021
     'otra cosa
+    'estoy en jorge
     Sub Main(args As String())
         Console.WriteLine("Hello World! SECOND")
         Console.ReadLine()
